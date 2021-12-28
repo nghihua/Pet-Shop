2e536887b7d796ed7909473373267724ed927880
nghihua <huanhatgianghi@gmail.com>
2021-12-28 23:16:04 +0700

    change font

--- src/main/java/gui/MainFrame.java
+++ src/main/java/gui/MainFrame.java
@@ -1,6 +1,7 @@
 package gui;
 
 import javax.swing.*;
+import javax.swing.border.EtchedBorder;
 import java.awt.*;
 import java.awt.event.*;
 
@@ -24,6 +25,8 @@ public class MainFrame extends JFrame implements ActionListener {
 
     //constructor to set properties of frame
     MainFrame() {
+        setUIFont(new javax.swing.plaf.FontUIResource("Serif",Font.PLAIN,20));
+
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setSize(800,500);
         this.setTitle("Pet Shop");
@@ -31,6 +34,7 @@ public class MainFrame extends JFrame implements ActionListener {
 
         initMenu();
         this.setJMenuBar(menuBar);
+        menuBar.setPreferredSize(new Dimension(100,40));
 
         this.setLocationRelativeTo(null);
         this.setVisible(true);
@@ -65,12 +69,15 @@ public class MainFrame extends JFrame implements ActionListener {
         menuBar.add(customerMenu);
     }
 
-    //initialize fonts for the whole GUI
-    void initFont() {
-//        UIManager.put("MenuItem.font", new Font("Times New Roman", Font.PLAIN, 20));
-//        UIManager.put("Menu.font", new Font("Times New Roman", Font.PLAIN, 20));
-        UIManager.getLookAndFeelDefaults()
-                .put("defaultFont", new Font("Arial", Font.BOLD, 25));
+    //method for setting the font for the whole GUI
+    void setUIFont (javax.swing.plaf.FontUIResource f){
+        java.util.Enumeration keys = UIManager.getDefaults().keys();
+        while (keys.hasMoreElements()) {
+            Object key = keys.nextElement();
+            Object value = UIManager.get (key);
+            if (value instanceof javax.swing.plaf.FontUIResource)
+                UIManager.put (key, f);
+        }
     }
 
     @Override
@@ -87,74 +94,3 @@ public class MainFrame extends JFrame implements ActionListener {
     }
 
 }
-
-//class AddPetFrame extends JFrame {
-//
-//    GridBagLayout gb = new GridBagLayout();
-//    GridBagConstraints gbc = new GridBagConstraints();
-//
-//    AddPetFrame(JFrame parentFrame) {
-//        this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-//        this.setSize(300,400);
-//        this.setTitle("Add New Pet");
-//        this.setLayout(gb);
-//        this.setLocationRelativeTo(null);
-//
-//        //make user cannot interact with the parent frame until closing this frame
-//        parentFrame.setFocusableWindowState(false);
-//        parentFrame.setEnabled(false);
-//        this.addWindowListener(new java.awt.event.WindowAdapter() {
-//            @Override
-//            public void windowClosed(java.awt.event.WindowEvent windowEvent) {
-//                parentFrame.setFocusableWindowState(true);
-//                parentFrame.setEnabled(true);
-//            }
-//        });
-//
-//        JLabel nameLabel = new JLabel("Name:");
-//        addComponent(this, nameLabel, 0, 0, 20, 30, 2, 30);
-//
-//        JTextField nameTextField = new JTextField();
-//        addComponent(this, nameTextField, 0, 1, 2, 30, 2, 30);
-//
-//        JLabel speciesLabel = new JLabel("Species:");
-//        addComponent(this, speciesLabel, 0, 2, 2, 30, 2, 30);
-//
-//        String[] petSpecies = { "Poodles", "Main Coon", "British Short Hair", "Golden Retriever", "Siamese" };
-//        JComboBox speciesComboBox = new JComboBox(petSpecies);
-//        speciesComboBox.setSelectedIndex(4);
-//        addComponent(this, speciesComboBox, 0, 3, 2, 30, 2, 30);
-//
-//
-//        JLabel ageLabel = new JLabel("Age:");
-//        addComponent(this, ageLabel, 0, 4, 2, 30, 2, 30);
-//        JTextField ageTextField = new JTextField();
-//        addComponent(this, ageTextField, 0, 5, 2, 30, 2, 30);
-//
-//        JLabel priceLabel = new JLabel("Price in:");
-//        addComponent(this, priceLabel, 0, 6, 2, 30, 2, 30);
-//        JTextField priceTextField = new JTextField();
-//        addComponent(this, priceTextField, 0, 7, 2, 30, 2, 30);
-//
-//        JButton submitButton = new JButton("Submit");
-//        addComponent(this, submitButton, 0, 8, 20, 100, 20, 100);
-//
-//        this.setVisible(true);
-//        //pack();
-//    }
-//
-//    void addComponent(Container container, Component component, int gridx, int gridy, int top, int left, int bottom, int right) {
-//        gbc.gridx = gridx;
-//        gbc.gridy = gridy;
-//
-//        gbc.gridwidth = 1;
-//        gbc.gridheight = 1;
-//
-//        gbc.fill = GridBagConstraints.BOTH;
-//
-//        gbc.insets = new Insets(top,left,bottom,right);
-//
-//        gb.setConstraints(component, gbc);
-//        container.add(component);
-//    }
-//}
