dd36856dac93a7024a5ac38daa76d5935fbb4124
nghihua <huanhatgianghi@gmail.com>
2022-01-01 12:32:45 +0700

    handle customer combobox listener

--- src/main/java/gui/SellPetDialog.java
+++ src/main/java/gui/SellPetDialog.java
@@ -28,18 +28,39 @@ public class SellPetDialog extends JDialog {
         //combo box
         customerComboBoxModel = new DefaultComboBoxModel();
         customerComboBox.setModel(customerComboBoxModel);
+
         //load initial data
         refreshCustomerComboBox(new String[]{"091738174", "09213810"});
+        //by default, the first customer in the list is selected
+        //load that customer's discount value and calculate the price here
+        String customer = customerComboBox.getSelectedItem().toString();
+        discountValueLabel.setText("0.2");
+        priceValueLabel.setText("100");
 
         //action listeners
+
+        //everytime reselect customer, this method is fired
+        customerComboBox.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                //get currently selected customer, load the discount value and calculate the price
+                String customer = customerComboBox.getSelectedItem().toString();
+                discountValueLabel.setText("0.2");
+                priceValueLabel.setText("100");
+            }
+        });
+
         confirmButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent actionEvent) {
                 String customer = customerComboBox.getSelectedItem().toString();
 
+                //insert into transaction table here
+
                 //catch error and if no error, please do this
                 JOptionPane.showMessageDialog(null, "Sell successfully!", "Congrats",
                         JOptionPane.PLAIN_MESSAGE);
+
                 //close sell pet and view pet dialogs after sold
                 SellPetDialog.this.dispose();
                 parent.dispose();
--- src/main/java/gui/SellSupplyDialog.java
+++ src/main/java/gui/SellSupplyDialog.java
@@ -4,8 +4,6 @@ import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-import static java.lang.Integer.parseInt;
-
 public class SellSupplyDialog extends JDialog {
 
     private JPanel mainPanel;
@@ -25,21 +23,43 @@ public class SellSupplyDialog extends JDialog {
         this.setContentPane(mainPanel);
         this.setLocationRelativeTo(null);
 
-        //combo box
+        //combo box model
         customerComboBoxModel = new DefaultComboBoxModel();
         customerComboBox.setModel(customerComboBoxModel);
-        //load initial data
+
+        //load customers
         refreshCustomerComboBox(new String[]{"091738174", "09213810"});
 
+        //by default, the first customer in the list is selected
+        //load that customer's discount value and calculate the price here
+        String customer = customerComboBox.getSelectedItem().toString();
+        discountValueLabel.setText("0.2");
+        priceValueLabel.setText("100");
+
         //action listeners
+
+        //everytime reselect customer, this method is fired
+        customerComboBox.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                //get currently selected customer, load the discount value and calculate the price
+                String customer = customerComboBox.getSelectedItem().toString();
+                discountValueLabel.setText("0.2");
+                priceValueLabel.setText("100");
+            }
+        });
+
         confirmButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent actionEvent) {
                 String customer = customerComboBox.getSelectedItem().toString();
 
+                //insert into transaction table here
+
                 //catch error and if no error, please do this
                 JOptionPane.showMessageDialog(null, "Sell successfully!", "Congrats",
                         JOptionPane.PLAIN_MESSAGE);
+
                 //close sell pet and view pet dialogs after sold
                 SellSupplyDialog.this.dispose();
                 parent.dispose();
