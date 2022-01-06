c7554e76c90fbaf475dddca274f9f78871c1db75
minhd <minhdeptrai4869@gmail.com>
2022-01-06 19:13:25 +0700

    add new classes and modify check pet dialog

--- src/main/java/objects/pets/Pets.java
+++ src/main/java/objects/pets/Pets.java
@@ -12,7 +12,7 @@ public class Pets {
     protected String species;
     protected String breed;
     protected double price_in;
-    Pets(String name, int age, double price_in)
+    protected Pets(String name, int age, double price_in)
     {
         this.name = name;
         this.age = age;
